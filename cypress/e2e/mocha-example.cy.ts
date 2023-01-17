e1edd3725c4eb390ca0c89c545a81dea5d611847
dunjans10 <91346284+dunjans10@users.noreply.github.com>
2023-01-17 01:18:48 +0100

    Skip it execution or only one it execute - example

--- cypress/e2e/mocha-example.cy.ts
+++ cypress/e2e/mocha-example.cy.ts
@@ -14,10 +14,10 @@ describe("empty spec", () => {
   it("testcase#1", () => {
     cy.log("Hello from test case #1")
   });
-  it("testcase#2", () => {
+  it.only("testcase#2", () => {
     cy.log("Hello from test case #2")
   });
-  it("testcase#3", () => {
+  it.skip("testcase#3", () => {
     cy.log("Hello from test case #3")
   });
 
