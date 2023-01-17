ee39f57dc09fe8bc5ed88496e89fc87f0f7fb81e
dunjans10 <91346284+dunjans10@users.noreply.github.com>
2023-01-17 01:39:52 +0100

    Adding first assertion

--- cypress/e2e/visit.cy.ts
+++ cypress/e2e/visit.cy.ts
@@ -5,6 +5,7 @@ describe("Basics - visit", () => {
   it("visit page text input", () => {
     cy.url().then((url) => {
       cy.log("Printing the URL: " + url)
+      expect(url).to.contains("/textinput")
     })
   
   });
