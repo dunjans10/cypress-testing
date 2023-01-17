bd422226460e0cee14a4a9fc17ff7882817ec2c5
dunjans10 <91346284+dunjans10@users.noreply.github.com>
2023-01-17 01:10:57 +0100

    Adding mocha example - describe and it

---
+++ cypress/e2e/mocha-example.cy.ts
@@ -0,0 +1,12 @@
+describe("empty spec", () => {
+  it("testcase#1", () => {
+    cy.log("Hello from test case #1")
+  });
+  it("testcase#2", () => {
+    cy.log("Hello from test case #2")
+  });
+  it("testcase#3", () => {
+    cy.log("Hello from test case #3")
+  });
+
+})
