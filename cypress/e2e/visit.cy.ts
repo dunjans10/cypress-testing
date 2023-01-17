97f1528c7d4efafbe5d15983ac58706b83bf9e1d
dunjans10 <91346284+dunjans10@users.noreply.github.com>
2023-01-17 01:34:37 +0100

    Yield subject using then command a chain

--- cypress/e2e/visit.cy.ts
+++ cypress/e2e/visit.cy.ts
@@ -3,7 +3,10 @@ describe("Basics - visit", () => {
     cy.visit("/textinput")
   })
   it("visit page text input", () => {
-   cy.log("Hello from it")
+    cy.url().then((url) => {
+      cy.log("Printing the URL: " + url)
+    })
+  
   });
   it("visit page text input", () => {
     cy.log("Hello from it")
