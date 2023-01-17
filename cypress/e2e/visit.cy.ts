ce9b2e0f4f1202c4e1ae31c00e5484f826e716d0
dunjans10 <91346284+dunjans10@users.noreply.github.com>
2023-01-17 01:43:18 +0100

    Checking title validation

--- cypress/e2e/visit.cy.ts
+++ cypress/e2e/visit.cy.ts
@@ -2,14 +2,22 @@ describe("Basics - visit", () => {
   beforeEach(() => {
     cy.visit("/textinput")
   })
+
   it("visit page text input", () => {
     cy.url().then((url) => {
       cy.log("Printing the URL: " + url)
       expect(url).to.contains("/textinput")
     })
-  
   });
+
   it("visit page text input", () => {
     cy.log("Hello from it")
    });
+
+  it("title validation", () => {
+    cy.title().then((title) => {
+      cy.log(title)
+      expect(title).to.be.equal("Text Input")
+    })
+  })
 })
