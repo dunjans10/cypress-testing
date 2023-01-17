8058b078890de0884c2101d26a7adcfcd322fa31
dunjans10 <91346284+dunjans10@users.noreply.github.com>
2023-01-17 02:07:07 +0100

    Find elements in DOM using cy.contains, cy.get and cy.find

---
+++ cypress/e2e/css-selectors.cy.ts
@@ -0,0 +1,13 @@
+describe("Locators", () => {
+  beforeEach(() => {
+    cy.visit("/dynamicid")
+  })
+
+  it("cy.contains example (find elements in DOM, UI", () => {
+    cy.contains("Button with Dynamic ID")
+  });
+
+  it("cy.get - for parent and cy.find - for children example", () => {
+    cy.get("div").find("button").should("have.text", "Button with Dynamic ID")
+  })
+})
