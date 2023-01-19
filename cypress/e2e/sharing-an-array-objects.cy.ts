80d99141c6b5109563eb255d631a5cc90e2625ab
dunjans10 <91346284+dunjans10@users.noreply.github.com>
2023-01-19 18:28:58 +0100

    Adding example - array of objects applied to a test scenario

--- cypress/e2e/sharing-an-array-objects.cy.ts
+++ cypress/e2e/sharing-an-array-objects.cy.ts
@@ -11,3 +11,24 @@ describe("Array of objects-Users", () => {
     })
   })
 })
+
+//array of objects applied to a test scenario
+
+describe.only("Array of objects-Users", () => {
+  it("Getting the information based on my fixture", () => {
+    cy.fixture<{users:User[]}>("example.json").its("users").then((users) => {
+      //cy.log(users)
+      users.forEach(user => {
+        cy.visit(`${Cypress.env("demoQA")}/login`)
+        cy.login(user.username, user.password)
+        if(user.valid){
+          cy.url().should("contain", "profile")
+        }else{
+          cy.url().should("contain", "login")
+        }
+        cy.clearCookies()
+        cy.clearLocalStorage()
+      })
+    })
+  })
+})
